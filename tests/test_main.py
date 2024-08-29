a9ea3e27a2f883102bd8ca497cbd39c8928591b0
Best Olunusi <olunusibest@gmail.com>
2024-08-29 18:37:09 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,9 +4,10 @@ from src.main import add, subtract
 
 class TestMain(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(1, 0), 3)
+        self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(-1, 1), 0)
 
+    
     def test_subtract(self):
         self.assertEqual(subtract(2, 1), 1)
         self.assertEqual(subtract(2, 2), 0)
